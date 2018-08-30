0171d74c33157586a20c3faff5c2ccfbe9271157
Mike Choi <mike.choi@live.ca>
2018-08-29 19:44:16 -0700

    Added key press data for color and sound in keyData.js, and animations now work on keypresses for a-z

---
+++ js/clone.js
@@ -0,0 +1,24 @@
+var circles = [];
+
+function onKeyDown(event) {
+    var sound = keyData[event.key].sound.play();
+
+    var maxPoint = new Point(view.size.width, view.size.height);
+    var rndPoint = Point.random() * (maxPoint+1);
+    
+    var myCircle = new Path.Circle(rndPoint, 300);
+    myCircle.fillColor = keyData[event.key].color;
+
+    circles.push(myCircle);
+}
+
+function onFrame(event) {
+    for(var i = 0; i < circles.length; i++){
+        circles[i].fillColor.hue += 1;
+        circles[i].scale(0.95);
+        if(circles[i].area < 1){
+            circles[i].remove();
+            circles.splice(i,1);
+        }
+    }
+}
---
+++ js/keyData.js
@@ -0,0 +1,106 @@
+var keyData = {
+    a: {
+        color: "#27c15f",
+        sound: new Howl( {src: ['Sounds/bubbles.mp3']} )
+    },
+    b: {
+        color: "#2a054f",
+        sound: new Howl( {src: ['Sounds/clay.mp3']} )
+    },
+    c: {
+        color: "#bd3db8",
+        sound: new Howl( {src: ['Sounds/confetti.mp3']} )
+    },
+    d: {
+        color: "#ec690b",
+        sound: new Howl( {src: ['Sounds/corona.mp3']} )
+    },
+    e: {
+        color: "#d1eb3b",
+        sound: new Howl( {src: ['Sounds/dotted-spiral.mp3']} )
+    },
+    f: {
+        color: "#0828dc",
+        sound: new Howl( {src: ['Sounds/flash-1.mp3']} )
+    },
+    g: {
+        color: "#f6c097",
+        sound: new Howl( {src: ['Sounds/flash-2.mp3']} )
+    },
+    h: {
+        color: "#ff59d4",
+        sound: new Howl( {src: ['Sounds/glimmer.mp3']} )
+    },
+    i: {
+        color: "#42d8e9",
+        sound: new Howl( {src: ['Sounds/flash-3.mp3']} )
+    },
+    j: {
+        color: "#824019",
+        sound: new Howl( {src: ['Sounds/moon.mp3']} )
+    },
+    k: {
+        color: "#d40e2e",
+        sound: new Howl( {src: ['Sounds/pinwheel.mp3']} )
+    },
+    l: {
+        color: "#89922f",
+        sound: new Howl( {src: ['Sounds/piston-1.mp3']} )
+    },
+    m: {
+        color: "#b2494e",
+        sound: new Howl( {src: ['Sounds/prism-1.mp3']} )
+    },
+    n: {
+        color: "#77c306",
+        sound: new Howl( {src: ['Sounds/splits.mp3']} )
+    },
+    o: {
+        color: "#894c7f",
+        sound: new Howl( {src: ['Sounds/squiggle.mp3']} )
+    },
+    p: {
+        color: "#f2093b",
+        sound: new Howl( {src: ['Sounds/strike.mp3']} )
+    },
+    q: {
+        color: "#5340dd",
+        sound: new Howl( {src: ['Sounds/suspension.mp3']} )
+    },
+    r: {
+        color: "#06220e",
+        sound: new Howl( {src: ['Sounds/timer.mp3']} )
+    },
+    s: {
+        color: "#e442a6",
+        sound: new Howl( {src: ['Sounds/ufo.mp3']} )
+    },
+    t: {
+        color: "#ee790c",
+        sound: new Howl( {src: ['Sounds/veil.mp3']} )
+    },
+    u: {
+        color: "#1cc70c",
+        sound: new Howl( {src: ['Sounds/wipe.mp3']} )
+    },
+    v: {
+        color: "#969324",
+        sound: new Howl( {src: ['Sounds/zig-zag.mp3']} )
+    },
+    w: {
+        color: "#d2cce6",
+        sound: new Howl( {src: ['Sounds/prism-3.mp3']} )
+    },
+    x: {
+        color: "#c9f8dc",
+        sound: new Howl( {src: ['Sounds/prism-2.mp3']} )
+    },
+    y: {
+        color: "#ef2cbb",
+        sound: new Howl( {src: ['Sounds/piston-2.mp3']} )
+    },
+    z: {
+        color: "#0ddc62",
+        sound: new Howl( {src: ['Sounds/piston-3.mp3']} )
+    }
+};
